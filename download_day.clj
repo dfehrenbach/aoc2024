b0514d0f93dac64e403e3957b9050211884e824a
Daniel Fehrenbach <dfehrenbach04@gmail.com>
2024-12-02 20:05:50 -0600

    Update the download day script to create dirs and .clj file

--- download_day.clj
+++ download_day.clj
@@ -1,6 +1,7 @@
 #!/usr/bin/env bb
 
-(require '[babashka.http-client :as http])
+(require '[babashka.http-client :as http]
+         '[babashka.fs :as fs])
 
 (def session-cookie (System/getenv "AOC_SESSION"))
 
@@ -12,14 +13,16 @@
   (println "Downloading url:" url)
   (http/get url {:headers {"Cookie" session-cookie}}))
 
-(defn write-html [file html]
+(defn write-file [file text]
   (println "Writing file:" file)
-  (spit file html))
+  (spit file text))
 
 (let [[day] *command-line-args*]
   (when (empty? day)
-    (println "Usage: <url> <file>")
+    (println "Usage: <day>")
     (System/exit 1))
   (let [day (read-string day) dir (day-dir day)]
-    (write-html (str dir "/prompt.txt") (:body (get-url (day-url day))))
-    (write-html (str dir "/input.txt") (:body (get-url (input-url day))))))
+    (fs/create-dirs dir)
+    (write-file (str dir "/prompt.txt") (:body (get-url (day-url day))))
+    (write-file (str dir "/input.txt") (:body (get-url (input-url day))))
+    (write-file (str dir "/core.clj") (str "(ns day" day ".core (:require [clojure.string :as string]))"))))
